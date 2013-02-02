51278468dd5fdf7d1d3d48010dcade26966ab571
William Broach <while1eq1@gmail.com>
2013-02-01 22:13:20 -0500

    Fixed indentation for PEP-8 Compliance and readability, Updated README for use on Mac OSX

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ MegaFS
 ======
 
 A FUSE filesystem wrapper for Mega.
+
+
+Instructions (Mac OSX)
+=====================
+
+Prerequisites:
+    OSXFUSE     - http://osxfuse.github.com/
+    fuse-python - http://pypi.python.org/pypi/fuse-python
+
+1. Install OSXFUSE.
+2. Install fuse-python.
+
+
--- megautil.py
+++ megautil.py
@@ -2,31 +2,38 @@ import base64
 import binascii
 import struct
 
+
 def base64urldecode(data):
-  data += '=='[(2 - len(data) * 3) % 4:]
-  for search, replace in (('-', '+'), ('_', '/'), (',', '')):
-    data = data.replace(search, replace)
-  return base64.b64decode(data)
+    data += '=='[(2 - len(data) * 3) % 4:]
+    for search, replace in (('-', '+'), ('_', '/'), (',', '')):
+        data = data.replace(search, replace)
+    return base64.b64decode(data)
+
 
 def base64urlencode(data):
-  data = base64.b64encode(data)
-  for search, replace in (('+', '-'), ('/', '_'), ('=', '')):
-    data = data.replace(search, replace)
-  return data
+    data = base64.b64encode(data)
+    for search, replace in (('+', '-'), ('/', '_'), ('=', '')):
+        data = data.replace(search, replace)
+    return data
+
 
 def a32_to_str(a):
-  return struct.pack('>%dI' % len(a), *a)
+    return struct.pack('>%dI' % len(a), *a)
+
 
 def str_to_a32(b):
-  if len(b) % 4:
-    b += '\0' * (4 - len(b) % 4)
-  return struct.unpack('>%dI' % (len(b) / 4), b)
+    if len(b) % 4:
+        b += '\0' * (4 - len(b) % 4)
+    return struct.unpack('>%dI' % (len(b) / 4), b)
+
 
 def a32_to_base64(a):
-  return base64urlencode(a32_to_str(a))
+    return base64urlencode(a32_to_str(a))
+
 
 def base64_to_a32(s):
-  return str_to_a32(base64urldecode(s))
+    return str_to_a32(base64urldecode(s))
+
 
 def mpi2int(s):
-  return int(binascii.hexlify(s[2:]), 16)
+    return int(binascii.hexlify(s[2:]), 16)
